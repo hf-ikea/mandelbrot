843767169d2b83cf9c44c17fa8a43f4990130511
Giovanni Forma <bluehairguyiscool@gmail.com>
2023-05-18 19:26:48 -0700

    really simple mandelbrot generator

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class MandelbrotSet
+{
+    public static void Main()
+    {
+        Bitmap bmp = new Bitmap(1000, 1000);
+
+        double x0;
+        double y0;
+        double x;
+        double y;
+        double x2 = 0;
+        double y2 = 0;
+
+        int maxIterations = 200;
+        int i = 0;
+
+
+        List<Color> colorArray = GetGradients(Color.FromArgb(255, 255, 0, 0), Color.FromArgb(255, 0, 255, 255), maxIterations + 1);
+
+        for(int pixX = 0; pixX < bmp.Width; pixX++)
+        {
+            for(int pixY = 0; pixY < bmp.Height; pixY++)
+            {
+                x0 = (pixX - (0.5 * bmp.Width)) * 2;
+                y0 = (pixY - (0.5 * bmp.Height)) * 2;
+                x0 /= bmp.Width;
+                y0 /= bmp.Height;
+                x = 0;
+                y = 0;
+                i = 0;
+                x2 = 0;
+                y2 = 0;
+
+                while((x2 + y2) <= 4 && i < maxIterations)
+                {
+                    y = 2 * x * y + y0;
+                    x = x2 - y2 + x0;
+                    x2 = x * x;
+                    y2 = y * y;
+                    i++;
+                }
+
+                //Console.WriteLine("Real: " + x.ToString() + ", Imaginary: " + y.ToString());
+
+                bmp.SetPixel(pixX, pixY, colorArray[i]);
+            }
+        }
+
+        
+
+        bmp.Save("out.png", ImageFormat.Png);
+    }
+
+    public static List<Color> GetGradients(Color start, Color end, int steps)
+    {
+        List<Color> gradients = new List<Color>();
+        int stepA = ((end.A - start.A) / (steps - 1));
+        int stepR = ((end.R - start.R) / (steps - 1));
+        int stepG = ((end.G - start.G) / (steps - 1));
+        int stepB = ((end.B - start.B) / (steps - 1));
+
+        for (int i = 0; i < steps; i++)
+        {
+            gradients.Add(Color.FromArgb(start.A + (stepA * i),
+                                         start.R + (stepR * i),
+                                         start.G + (stepG * i),
+                                         start.B + (stepB * i)));
+        }
+
+        return gradients;
+    }
+}
