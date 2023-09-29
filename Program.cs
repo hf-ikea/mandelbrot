6d8750ac97bc810457655556005a64a28eaca699
Giovanni Forma <bluehairguyiscool@gmail.com>
2023-09-28 20:50:43 -0700

    simple parallel maybe

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Threading;
+using System.Drawing;
 using System.Drawing.Imaging;
 
 // https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
@@ -6,48 +8,63 @@ using System.Drawing.Imaging;
 
 public class MandelbrotSet
 {
-    public static void Main()
+    public readonly static int sizeX = 5000;
+    public readonly static int sizeY = 5000;
+    public readonly static int maxIteration = 200;
+
+    public static int[,]? iterationCounts;
+
+    public static void RenderLine(int LineNum, ref int[,] iterationCounts)
     {
-        int sizeX = 1000;
-        int sizeY = 1000;
-        int maxIterations = 200;
-
-        int[,] iterationCounts = new int[sizeX, sizeY];
-        int[] numIterationsPerPixel = new int[maxIterations + 1];
-        double[,] hue = new double[sizeX, sizeY];
-
-        List<Color> palette = GetGradients(Color.FromArgb(0, 57, 143), Color.FromArgb(0, 0, 0), maxIterations); // create linear pallete, works okay
-        Bitmap bmp = new Bitmap(sizeX, sizeY);
-
-        // loop through every pixel
         for(int pixX = 0; pixX < sizeX; pixX++)
         {
-            for(int pixY = 0; pixY < sizeY; pixY++)
+            //       translation ▼   size divisor ▼
+            double x0 = (pixX - (0.65 * sizeX)) * 3;
+            double y0 = (LineNum - (0.5  * sizeY)) * 3;
+            x0 /= sizeX;
+            y0 /= sizeY;
+            double x = 0;
+            double y = 0;
+            int i = 0;
+            double x2 = 0;
+            double y2 = 0;
+
+            // run until escape, save iteration count in array
+            while((x2 + y2) <= 4 && i < maxIteration)
             {
-                //       translation ▼   size divisor ▼
-                double x0 = (pixX - (0.65 * sizeX)) * 3;
-                double y0 = (pixY - (0.5  * sizeY)) * 3;
-                x0 /= sizeX;
-                y0 /= sizeY;
-                double x = 0;
-                double y = 0;
-                int i = 0;
-                double x2 = 0;
-                double y2 = 0;
-
-                // run until escape, save iteration count in array
-                while((x2 + y2) <= 4 && i < maxIterations)
-                {
-                    y = 2 * x * y + y0;
-                    x = x2 - y2 + x0;        // x goes after y to prevent wack iterations
-                    x2 = x * x;
-                    y2 = y * y;
-                    i++;
-                }
-
-                iterationCounts[pixX,pixY] = i;
+                y = 2 * x * y + y0;
+                x = x2 - y2 + x0;        // x goes after y to prevent wack iterations
+                x2 = x * x;
+                y2 = y * y;
+                i++;
             }
+
+            iterationCounts[pixX, LineNum] = i;
         }
+    }
+    public static void Main()
+    {
+        iterationCounts = new int[sizeX, sizeY];
+        int[] numIterationsPerPixel = new int[maxIteration + 1];
+        double[,] hue = new double[sizeX, sizeY];
+
+        List<Color> palette = GetGradients(Color.FromArgb(0, 57, 143), Color.FromArgb(0, 0, 0), maxIteration); // create linear pallete, works okay
+        Bitmap bmp = new Bitmap(sizeX, sizeY);
+
+        Console.WriteLine("Start escape algorithm");
+
+        // loop through every line
+        Parallel.For(0, sizeY, line => {
+            RenderLine(line, ref iterationCounts);  // Parallel
+        });
+
+        // for(int line = 0; line < sizeY; line++)
+        // {
+        //     RenderLine(line, ref iterationCounts);  // Single thread
+        // }
+        
+
+        Console.WriteLine("Finish escape algorithm\nStart Histogram");
 
         // Start histogram coloring
         for(int x = 0; x < sizeX; x++)
@@ -61,7 +78,7 @@ public class MandelbrotSet
 
         // calculate totals for normalization
         double total = 0;
-        for(int i = 0; i < maxIterations; i++)
+        for(int i = 0; i < maxIteration; i++)
         {
             total += numIterationsPerPixel[i];
         }
@@ -83,7 +100,7 @@ public class MandelbrotSet
         {
             for(int y = 0; y < sizeY; y++)
             {
-                bmp.SetPixel(x, y, palette[(int)Math.Round(Math.Pow(hue[x, y], 5) * (maxIterations - 1))]); // exponetial coloring is more pleasing
+                bmp.SetPixel(x, y, palette[(int)Math.Round(Math.Pow(hue[x, y], 5) * (maxIteration - 1))]); // exponetial coloring is more pleasing
             }
         } 
 
